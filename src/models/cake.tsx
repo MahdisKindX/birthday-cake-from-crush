c2ad470bf9bcd95f4e068decf4170b8e23103ab2
MahdisKindX <153658945+MahdisKindX@users.noreply.github.com>
2025-12-27 22:37:29 +0300

    optimized cake file

--- src/models/cake.tsx
+++ src/models/cake.tsx
@@ -2,13 +2,15 @@ import { useLoader } from "@react-three/fiber";
 import type { ThreeElements } from "@react-three/fiber";
 import { useMemo } from "react";
 import type { Group } from "three";
+import { MeshoptDecoder } from "three/examples/jsm/libs/meshopt_decoder.module.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 
 type CakeProps = ThreeElements["group"];
 
 export function Cake({ children, ...groupProps }: CakeProps) {
-  const gltf = useLoader(GLTFLoader, "/cake.glb");
-  const cakeScene = useMemo<Group | null>(() => gltf.scene?.clone(true) ?? null, [gltf.scene]);
+const gltf = useLoader(GLTFLoader, "/cake.min.glb", (loader) => {
+  loader.setMeshoptDecoder(MeshoptDecoder);
+});  const cakeScene = useMemo<Group | null>(() => gltf.scene?.clone(true) ?? null, [gltf.scene]);
 
   if (!cakeScene) {
     return null;
